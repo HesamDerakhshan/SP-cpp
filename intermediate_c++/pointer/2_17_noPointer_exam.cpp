e3d8fbafb3e2d58e2e4f3d7709330cb52c7c60d3
hesam <Hesam605@gmail.com>
2021-01-01 00:30:11 +0330

    work on examination small project

--- intermediate_c++/pointer/2_17_noPointer_exam.cpp
+++ intermediate_c++/pointer/2_17_noPointer_exam.cpp
@@ -13,47 +13,6 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-
-	float  **student;
-	int    *exam;
-
-	int n_student, count_stu;
-	cout<<"enter number student:";
-	cin >> n_student;
-
-	student = new  float *[n_student];
-	exam    = new  int    [n_student];  
-
-
-	for( count_stu =0 ; count_stu < n_student ; count_stu++ ){
-		cout << "\n\n\n enter number exam for student"<< count_stu+1 <<":";
-		cin  >> exam[count_stu];
-    	
-		student[count_stu] = new  float[exam[count_stu]];
-    	
-		int sum=0;
-		for( int count_exam=0 ; count_exam < exam[count_stu] ; count_exam++){
-    	    cout << " nomreh " << count_exam+1 << ":";
-			cin  >> student[count_stu][count_exam];
-			sum += student[count_stu][count_exam];
-		   }
-        cout<<"average= "<<(float) sum/ exam[count_stu];
-    }
- 
-    
-	for( count_stu=0 ; count_stu < n_student ; count_stu++)
-		delete [] student[count_stu];
-      
-	delete [] student;
-    delete [] exam;
-  return 0;
-}
-
-
-
-/*
 void avrage(int);
 int main()
 {
@@ -80,4 +39,3 @@ void avrage(int n){
 	}
 	cout <<"avrage: " << sum/n;
 }
-*/
